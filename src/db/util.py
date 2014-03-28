5162a9041f7073eb22a688107327271b6bf905e8
drmaas <drmaas@gmail.com>
2014-03-27 22:29:24 -0500

    use env url to connect to db

--- src/db/util.py
+++ src/db/util.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 
+import os
+
 '''
 Created on Mar 27, 2014
 
@@ -7,7 +9,9 @@ Created on Mar 27, 2014
 '''
 
 def psql_connect():
-    return create_engine('postgresql://coin:coin@localhost/coindb', echo=True)
+    dburl = os.environ['DATABASE_URL']
+    #localtest: postgresql://coin:coin@localhost/coindb
+    return create_engine(dburl, echo=True)
     
 def sqlite_connect():
     return create_engine('sqlite:////home/drmaas/projects/python/coin-data/coin.db', echo=True)

11a4653b253f8a9c58d325b3c874fabf4fb5705e
drmaas <drmaas@gmail.com>
2014-04-12 21:20:16 -0500

    some more data fixes

--- src/db/datasetup.py
+++ src/db/datasetup.py
@@ -7,6 +7,8 @@ Created on Apr 12, 2014
 from db.dao import ExchangeDao
 from db.dao import PairDao
 
+from db.coin import Coin
+
 from db.util import getSession
 
 def run():
@@ -26,6 +28,13 @@ def run():
     pairs = [ 'btc_usd', 'ltc_usd', 'ltc_btc', 'nmc_usd', 'nmc_btc', 'ppc_usd', 'ppc_btc', 'xpm_btc' ]
     for pair in pairs:
         pairDao.addPair(btceExchangeId, pair)
+    session.commit()
+    
+    # populate missing pairId for coin entries
+    coins = session.query(Coin)
+    tuples = { 'btc_usd':9, 'ltc_usd':10, 'ltc_btc':11, 'nmc_usd':12, 'nmc_btc':13, 'ppc_usd':14, 'ppc_btc':15, 'xpm_btc':16 }
+    for coin in coins:
+        coin.pairId = tuples[coin.pair]
     session.commit()
 
 if __name__ == '__main__':

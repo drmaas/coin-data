2a8899d8388f4b596f21442be0d46807ec86ebf0
drmaas <drmaas@gmail.com>
2014-04-13 21:46:40 -0500

    adding notes about how to perform trades

--- src/trade/trader.py
+++ src/trade/trader.py
@@ -12,9 +12,21 @@ class Trader(object):
         pass
     
     def buy(self, exchangeId, pair):
+        #0. Check if active orders exist for this pair - if so cancel.
+        #1. get account balance for the 2nd portion of the pair (screen scrape). If > 0 we will attempt a buy.
+        #2. get latest ask price for pair, that will be target price to buy at.
+        #3. the quantity to buy is balance/(buy price)
+        #4. Execute trade
+        #5. Add entry to trade table about what was decided
         pass
     
     def sell(self, exchangeId, pair):
+        #0. Check if active orders exist for this pair - if so cancel.
+        #1. get account balance for the 1st portion of the pair (screen scrape). If > 0 we will attempt a sell.
+        #2. get latest bid price for pair, that will be target price to sell at.
+        #3. the quantity to sell is balance
+        #4. Execute trade
+        #5. Add entry to trade table about what was decided        
         pass
 
 class TradeState(Enum):
